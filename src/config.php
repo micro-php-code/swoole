18eedb9916bb4b627ebcd076e441e145e2632da3
liutao <email.lt@qq.com>
2025-04-18 13:26:25 +0800

    feat: format

--- src/config.php
+++ src/config.php
@@ -8,19 +8,19 @@ return [
     // 不开启协程就如同workerman一样
     'enable_coroutine' => true,
     'open_tcp_nodelay' => true,
-    /**
+    /*
      * 设置当前工作进程最大协程数量
      * 在 Server 程序中实际最大可创建协程数量等于 worker_num * max_coroutine
      */
     'max_coroutine' => 10000,
-    /**
+    /*
      * 一个 worker 进程在处理完超过此数值的任务后将自动退出，进程退出后会释放所有内存和资源
      * 不会立刻停止, 停止时间取决于max_wait_time
      */
     'max_request' => 10000,
     'socket_buffer_size' => 1024 * 1024 * 2,
     'buffer_output_size' => 1024 * 1024 * 2,
-    /**
+    /*
      * 设置 Worker 进程收到停止服务通知后最大等待时间【默认值：3】
      */
     'max_wait_time' => 5,
